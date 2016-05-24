1ae07cd9672b05b1ea948f14ffdec37ae3bccd2f
byazit <byazit@gmail.com>
2016-05-24 10:45:18 +0200

    This is on a server

---
+++ Server/includes/footer.php
@@ -0,0 +1,75 @@
+<footer class="row">
+			<div>
+				<div class="span4">
+					<div class="is-padded">
+						<nav>
+							<h2>Navigation</h2>
+							<hr>
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Portfolio</a></li>
+								<li><a href="#">Contact</a></li>
+								<li><a href="#">About</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<div class="span4">
+					<div class="is-padded">
+						<h2>Twitter</h2>
+						<hr>
+						<a href="#">@AwfulMedia</a>
+						<p>This is what my tweet looks like on this website. Tweet tweet. Twitter Twatter.</p>
+						<em>3 days ago</em><br>
+						<br>
+						<a href="#">@AwfulMedia</a>
+						<p>This is what my tweet looks like on this website. Tweet tweet. Twitter Twatter.</p>
+						<em>3 days ago</em>
+					</div>
+				</div>
+				<div class="span4">
+					<div class="is-padded">
+				  <h2>Details</h2>
+						  <blockquote>Respond is a fully responsive website template utilizing Twitter's Bootstrap framework. 
+						  What does that mean? First of all, the website will respond according to the size of the 
+						  viewers browser window. Go on, try it out. Resize your window. On top of that, the framework 
+						  offers loads of ready-to-go features. Check it out with the button below.
+						  <br><br>
+						  <em>- AwfulMedia.com</em>
+						  </blockquote>
+					  </div>
+				</div>
+			</div>
+			<p>
+			&copy;2014 cse10bmi@cs.umu.se<br>
+			Something happening <a href="http://cs.umu.se/~cse10bmi">SpeakDevice</a>
+			</p>
+		</footer>
+    </div><!-- /container -->
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+	<script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+	<script src="js/jquery-ui-1.8.18.custom.min.js"></script>
+	<script src="js/jquery.smooth-scroll.min.js"></script>
+	<script src="js/lightbox.js"></script>
+		<script>
+$('.carousel').carousel({
+  interval: 5000
+})
+</script>
+  </body>
+</html>
