9792b46056fccc29b7ad1c5ca3e54cdd8a34bc08
byazit <byazit@gmail.com>
2016-10-04 16:52:15 +0200

    duplicate

--- Server/demo_sse.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache');
-
-$time = exec('cat /sys/class/thermal/thermal_zone0/temp');
-$time=$time/1000;
-echo "data:{$time}\n\n";
-flush();
-?>
