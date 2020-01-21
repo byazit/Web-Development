0da4f8661c42ec92b50310314b39b992c717f2ef
byazit@gmail.com <byazit@gmail.com>
2020-01-21 09:05:26 +0100

    react_node_mysql

---
+++ react/react_node_mysql/config.js
@@ -0,0 +1,15 @@
+var config = {
+    database: {
+        host:      '172.17.0.2',     // database host
+        user:       'byazit',         // your database username
+        password: 'byazit3417',         // your database password
+        port:       3306,         // default MySQL port
+        db:       'mynode'         // your database name
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+ 
+module.exports = config
